28528b4ef26f11aeee2124fa05b5c22887118945
ddamy <841312998@qq.com>
2020-11-22 17:33:33 +0800

    list

---
+++ 07-list.py
@@ -0,0 +1,5 @@
+people = ['张三', '李四', '王麻子']
+print('现在班里有', len(people), '个同学, 分别是:', people)
+myName = input('那么你叫什么呢？')
+people.append(myName)
+print('那么，现在班里有', len(people), '个同学, 分别是:', people)
